e4728663575a1a5787b1a2e7fd675bf9938301db
domchuy <jgp@nowaii.com>
2022-11-11 04:05:47 -0600

    Feat: Renderizado de productos directamente de bd

--- src/pages/producto/index.tsx
+++ src/pages/producto/index.tsx
@@ -8,7 +8,7 @@ import { trpc } from '../../utils/trpc'
 
 const ListaProductos = () => {
   const productos = trpc.producto.dataProductos.useQuery();
-  // console.log('<===>',productos)
+  console.log('<===>',productos.data?.productos)
   return (
     <>
       <div
@@ -43,34 +43,15 @@ const ListaProductos = () => {
               </Link>
             </div>
           </div>
-          <ListaElementos
-            productos={[
-              {
-                id: 1,
-                nombre: "xiaomi",
-                slug: 'xiaomi-prod',
-                descripcion: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de',
-                existencia: 2,
-                precio: 15.5
-              },
-              {
-                id: 2,
-                nombre: "alcatel",
-                slug: 'alcatel-prod',
-                descripcion: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de',
-                existencia: 2,
-                precio: 15.5
-              },
-              {
-                id: 3,
-                nombre: "samsung",
-                slug: 'samsung-prod',
-                descripcion: 'Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de',
-                existencia: 2,
-                precio: 15.5
-              }
-            ]}
-          />
+          {productos.data?.productos?(
+            <ListaElementos
+              productos={productos.data?.productos}
+            />
+          ):(
+            <>
+              <p className='text-lg'>Cargando...</p>
+            </>
+          )}
         </div>
       </div>
     </>
